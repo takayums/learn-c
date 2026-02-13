1c453da88a407107db1b25e3179cf575c3add707
takayums <takayums20@gmail.com>
2026-02-13 23:55:56 +0700

    initial commit learn c language

---
+++ 01-hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  // Statemen  1
+  printf("Hello World \n");
+  // Statemen 2
+  printf("Hoola Asraf \n");
+  // Statement 3
+  printf("Last Statement \n");
+  return 0;
+}
