b539d3ae85ca2aea7b04fb383d5dbfb2351c5223
takayums <takayums20@gmail.com>
2026-04-29 09:10:10 +0700

    boolean in c

---
+++ 09-boolean.c
@@ -0,0 +1,11 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+int main() {
+  bool isMe = true;
+  bool isYou = false;
+
+  printf("Is This Are You: %d \n", isYou);
+  printf("Are you sure %d \n", isMe);
+  return 0;
+}
