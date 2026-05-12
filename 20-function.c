5d67b8d42847defb4de82a3f390802a30a33d442
takayums <takayums20@gmail.com>
2026-05-12 08:32:56 +0700

    scope variable and declaration function in c

--- 20-function.c
+++ 20-function.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+// declaretion function
+void myFunction();
+int Sum(int x, int y);
+
 // Declare Function
+// Definition function
 void myFunction() { printf("Hello Wolrd\n"); }
 
 // Function Parameteers
@@ -9,6 +14,16 @@ void SayHello(char name[]) { printf("Hello %s \n", name); }
 // Function Multiple Parameters
 void Introduce(char name[], int age) {
   printf("Hello my name is %s, I %d years old \n", name, age);
+}
+
+// global variable
+int z = 10;
+
+// Definition Function
+int Sum(int x, int y) {
+  // local variable
+  int result = x + y;
+  return result;
 }
 
 int main() {
@@ -20,5 +35,9 @@ int main() {
   SayHello("Dikiy");
 
   Introduce("Asraf", 25);
+
+  int result = Sum(3, 5);
+  printf("%d \n", result);
+
   return 0;
 }
